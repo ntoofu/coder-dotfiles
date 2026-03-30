14a1a73e5c1fa10169db938bbee1073cdb6d2f35
ntoofu <ntoofu@users.noreply.github.com>
2026-03-30 01:09:08 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # coder-dotfiles
 Dotfiles repository for Coder workspaces
+
+## install
+
+Run `install.sh`
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+git config --global user.name "ntoofu"
+git config --global user.email "ntoofu@noreply.github.com"
