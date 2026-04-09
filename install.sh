c757efe8ab27a14f37c18cfbb3fd29ca31b2668b
ntoofu <ntoofu@users.noreply.github.com>
2026-04-10 03:15:26 +0900

    Add Claude configuration to install script

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 
 git config --global user.name "ntoofu"
 git config --global user.email "ntoofu@users.noreply.github.com"
+mkdir -p ~/.claude || :
+cp claude-settings-to-ignore-claude-md-provided-by-coder.json ~/.claude
+echo "alias 'claude=claude --settings ~/.claude/claude-settings-to-ignore-claude-md-provided-by-coder.json'" >> ~/.bashrc
